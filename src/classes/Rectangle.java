1dfe83a4988b59b4c3d34f069b21e3c67d543b8c
1594003권미정 <j7jjj777@naver.com>
2016-03-29 14:47:04 +0900

    Update Rectangle.java

--- src/classes/Rectangle.java
+++ src/classes/Rectangle.java
@@ -1,44 +1,74 @@
-package classes;
+//package classes;
 
 public class Rectangle {
     private int x1, y1, x2, y2;
 
     public void setPoints(int _x1, int _y1, int _x2, int _y2) {
         // TODO, check if x, y > 0
-        x1 = _x1;
-        y1 = _y1;
-        x2 = _x2;
-        y2 = _y2;
+
+        int x; int y;
+        if(_x1>0 &&_y1>0 &&_x2>0 &&_y2>0)
+        {
+            if(_x1>=_x2) {
+                x2 = _x1;
+                x1 = _x2;
+            }
+            else if(_x1<_x2){
+                x1 = _x1;
+                x2 = _x2;
+            }
+
+            if(_y1>=_y2){
+                y2 = _y1;
+                y1 = _y2;}
+            else if(_y1<_y2){
+                y1 = _y1;
+                y2 = _y2;}
+        }
+
     }
 
     public int getWidth() {
+        int Width;
+
+        Width=x2-x1;
         // TODO
-        return 0;
+        return Width;
     }
 
     public int getHeight() {
+        int Height;
+
+        Height=y2-y1;
         // TODO
-        return 0;
+        return Height;
     }
 
     public int getArea() {
+
+        int Area;
+
+        Area=(getHeight()*getWidth());
         // TODO
-        return 0;
+        return Area;
     }
 
     public boolean equals(Rectangle r) {
-        // TODO
-        return false;
+        if((x1==r.x1) && (x2==r.x2) && (y1==r.y1) && (y2==r.y2))
+            return true;
+        else return false;
     }
 
     public boolean in(Rectangle r) {
-        // TODO
-        return false;
+        if((r.x1<=x1&&x1<=r.x2) && (r.x1<=x2&&x2<=r.x2) && (r.y1<=y1&&y1<=r.y2) && (r.y1<=y2&&y2<=r.y2))
+            return true;
+        else return false;
     }
 
     public boolean overlap(Rectangle r) {
-        // TODO
-        return false;
+        if(((x1<=r.x1&&r.x1<=x2) || (x1<=r.x2&&r.x2<=x2)) && ((y1<=r.y1&&r.y1<=y2) || (y1<=r.y2&&r.y2<=y2)))
+            return true;
+        else return false;
     }
 
     public static void main(String[] args) {
@@ -68,3 +98,4 @@ public class Rectangle {
     }
 
 }
+
