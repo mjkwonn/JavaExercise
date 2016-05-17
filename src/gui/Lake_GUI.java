a4f2f3e05ff2c8b0a16c3a96b7e5e5b2cc2975df
1594003권미정 <j7jjj777@naver.com>
2016-05-17 14:08:53 +0900

    Update Lake_GUI.java

--- src/gui/Lake_GUI.java
+++ src/gui/Lake_GUI.java
@@ -4,23 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
-/**
- * Created by jyheo on 2016-05-17.
- */
+
 
 abstract class MyObject extends JButton {
     protected String name;
     protected String shape;
-    protected int x, y;
+   // protected int x, y;
     public MyObject(String name, String shape, int x, int y) {
         super(shape);
-        setLocation(x, y);
+        setLocation(getX(), getY());
         setSize(70, 30);
         setVisible(true);
         this.name = name;
         this.shape = shape;
-        this.x = x;
-        this.y = y;
+
     }
 
     public void new_move(int width, int height) {};
@@ -34,6 +31,7 @@ class MyRock extends MyObject {
 
 class MyFish extends MyObject {
     private int velocity_x = 10;
+    private int velocity_y = 10;
 
     public MyFish(String name, String shape, int x, int y) {
         super(name, shape, x, y);
@@ -41,12 +39,16 @@ class MyFish extends MyObject {
 
     public void new_move(int width, int height) {
         // getX(), getY()
-
+        int x=getX(), y=getY();
         double rand = Math.random();
         if (rand < 0.5)
-            x += velocity_x;
+        { x += velocity_x;}
+
+
         else
-            y += 5;
+        { y+=velocity_y;}
+
+
         if (x + getWidth() >= width) {
             x = width - getWidth();
             velocity_x = -velocity_x;
@@ -54,15 +56,22 @@ class MyFish extends MyObject {
             x = 0;
             velocity_x = -velocity_x;
         }
-        if (y >= height)
+
+        if (y + getHeight() >= height) {
+            y = height - getHeight();
+            velocity_y = -velocity_y;
+        } else if (y <= 0) {
             y = 0;
+            velocity_y = -velocity_y;
+        }
+
         setLocation(x, y);
     }
 }
 
 public class Lake_GUI extends JFrame {
-    private int width;
-    private int height;
+    //private int width;
+    //private int height;
     private ArrayList<MyObject> objects = new ArrayList<>();
 
     public Lake_GUI(int width, int height) {
@@ -72,8 +81,6 @@ public class Lake_GUI extends JFrame {
         setSize(width, height);
         setVisible(true);
 
-        this.width = width;
-        this.height = height;
     }
 
     public void addMyObject(MyObject obj) {
@@ -82,16 +89,12 @@ public class Lake_GUI extends JFrame {
     }
 
     public void moveObjects() {
-        for (MyObject obj : objects) {
-            obj.new_move(width, height);
+        for (Component c : getContentPane().getComponents())
+            ((MyObject)c).new_move(getWidth(), getHeight());
         }
 
-        // getContentPane().getWidth(), getHeight()
+   
 
-        //for (Component c : getContentPane().getComponents()) {
-        //
-        //}
-    }
 
     public static void main(String args[]) throws InterruptedException {
         Lake_GUI lake = new Lake_GUI(320, 240);
